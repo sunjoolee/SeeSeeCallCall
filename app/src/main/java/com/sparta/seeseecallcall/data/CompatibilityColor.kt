a5d4d4fc8ff59dbe976e56125de75c134481e0a6
sunjoolee <sheisthepoem@naver.com>
2024-01-16 20:49:37 +0900

    Feat: MBTI 궁합 정보 생성

---
+++ app/src/main/java/com/sparta/seeseecallcall/data/CompatibilityColor.kt
@@ -0,0 +1,11 @@
+package com.sparta.seeseecallcall.data
+
+import com.sparta.seeseecallcall.R
+
+enum class CompatibilityColor(val color: Int) {
+    BEST(R.color.mbti_best),
+    GOOD(R.color.mbti_good),
+    SOSO(R.color.mbti_soso),
+    BAD(R.color.mbti_bad),
+    UN_KNOWN(R.color.gray)
+}
