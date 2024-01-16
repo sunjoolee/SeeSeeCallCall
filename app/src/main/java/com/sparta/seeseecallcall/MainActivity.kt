3ff3d374b331bc04e8849a1922e69298e493099f
nhkim <nhkim1529@gmail.com>
2024-01-16 14:11:00 +0900

    Feat: viewpager 코드 추가

--- app/src/main/java/com/sparta/seeseecallcall/MainActivity.kt
+++ app/src/main/java/com/sparta/seeseecallcall/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sparta.seeseecallcall.databinding.ActivityMainBinding
 
@@ -15,8 +16,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment)
+        val viewPager: ViewPager2 = binding.viewPager
+        viewPager.adapter = ViewPagerAdapter(this)
 
-        navView.setupWithNavController(navController)
+        navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_contact -> viewPager.currentItem = 0
+                R.id.navigation_bookmark -> viewPager.currentItem = 1
+                R.id.navigation_myPage -> viewPager.currentItem = 2
+            }
+            true
+        }
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                binding.navView.menu.getItem(position).isChecked = true
+            }
+        })
+
     }
 }
