39390da61cf5faeb1185cde9a7808f66c1261c03
nhkim <nhkim1529@gmail.com>
2024-01-16 11:11:31 +0900

    Feat: bottomNavigation 추가

--- app/src/main/java/com/sparta/seeseecallcall/MainActivity.kt
+++ app/src/main/java/com/sparta/seeseecallcall/MainActivity.kt
@@ -2,10 +2,21 @@ package com.sparta.seeseecallcall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sparta.seeseecallcall.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        navView.setupWithNavController(navController)
     }
 }
