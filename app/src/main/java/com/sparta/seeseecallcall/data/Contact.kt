556b60d1e7039702eb08043d9fca047df8be821c
sunjoolee <sheisthepoem@naver.com>
2024-01-16 11:35:12 +0900

    Feat: 디폴트로 사용할 연락처 정보 List 생성

--- app/src/main/java/com/sparta/seeseecallcall/data/Contact.kt
+++ app/src/main/java/com/sparta/seeseecallcall/data/Contact.kt
@@ -6,21 +6,8 @@ data class Contact(
     val profileImage: Uri? = null,
     val name: String,
     val phoneNumber:String,
-    val birthDate:Array<Int>,
+    val email:String,
+    val birthDate:String,
     val memo:String,
     var favorite:Boolean = false
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-
-        if (javaClass != other?.javaClass) return false
-        other as Contact
-
-        if (!birthDate.contentEquals(other.birthDate)) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return birthDate.contentHashCode()
-    }
-}
+) {}
