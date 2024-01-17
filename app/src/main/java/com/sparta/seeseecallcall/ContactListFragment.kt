7e7e733cde5183e6cf8bf80750f2ac5919d0ee23
sunjoolee <sheisthepoem@naver.com>
2024-01-17 11:35:59 +0900

    Feat: 연락처 이름 검색, mbti 검색 기능 추가

--- app/src/main/java/com/sparta/seeseecallcall/ContactListFragment.kt
+++ app/src/main/java/com/sparta/seeseecallcall/ContactListFragment.kt
@@ -1,6 +1,8 @@
 package com.sparta.seeseecallcall
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.sparta.seeseecallcall.data.ContactManager
+import com.sparta.seeseecallcall.data.Contact
 import com.sparta.seeseecallcall.data.ContactManager.contactBookmarkList
 import com.sparta.seeseecallcall.data.ContactManager.contactList
 import com.sparta.seeseecallcall.databinding.FragmentContactListBinding
@@ -27,19 +29,24 @@ class ContactListFragment : Fragment() {
         val adapter = MyAdapter(contactList)
         binding.recyclerviewList.adapter = adapter
         binding.recyclerviewList.layoutManager = LinearLayoutManager(context)
-        binding.recyclerviewList.addItemDecoration(DividerItemDecoration(context, LinearLayout.VERTICAL))
+        binding.recyclerviewList.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                LinearLayout.VERTICAL
+            )
+        )
 
-        adapter.itemClick = object : MyAdapter.ItemClick{
+        adapter.itemClick = object : MyAdapter.ItemClick {
             override fun onClick(view: View, position: Int) {
                 Log.d(TAG, "position: $position")
                 //TODO 연락처 객체 넘기며, 연락처 상세 페이지로 이동
             }
 
             override fun onStarClick(view: View, position: Int) {
-                contactList[position].run{
+                contactList[position].run {
                     favorite = !favorite
 
-                    if(favorite) contactBookmarkList.add(this)
+                    if (favorite) contactBookmarkList.add(this)
                     else contactBookmarkList.remove(this)
                 }
                 contactBookmarkList.sortBy { it.name }
@@ -47,13 +54,37 @@ class ContactListFragment : Fragment() {
             }
         }
 
+        binding.etSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(text: Editable?) {
+                Log.d(TAG, "afterTextChanged, ${text.toString()}")
+                adapter.ChangeDataset(
+                    if (text.isNullOrBlank()) contactList
+                    else getFilteredList(text.toString())
+                )
+            }
+        })
+
         return binding.root
     }
 
-    override fun onResume(){
+    override fun onResume() {
         Log.d(TAG, "ContactListFragmentList onResume()")
 
         this.view?.findViewById<RecyclerView>(R.id.recyclerview_list)?.adapter?.notifyDataSetChanged()
         super.onResume()
+    }
+
+    private fun getFilteredList(searchText: String): MutableList<Contact> {
+        val filteredList = mutableListOf<Contact>()
+        return filteredList.apply {
+            contactList.forEach {
+                //이름으로 검색
+                if (it.name.contains(searchText)) add(it)
+                //mbti로 검색
+                if (it.mbti.contains(searchText.toUpperCase())) add(it)
+            }
+        }
     }
 }
