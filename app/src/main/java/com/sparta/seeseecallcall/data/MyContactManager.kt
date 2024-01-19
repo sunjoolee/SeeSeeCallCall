f9b1138aa08fc74a91fc71b7b124e206d6a0ff12
hwoo4013 <hwoo4013@hanmail.net>
2024-01-19 10:02:04 +0900

    design: mbti 폰트 색상 변경

--- app/src/main/java/com/sparta/seeseecallcall/data/MyContactManager.kt
+++ app/src/main/java/com/sparta/seeseecallcall/data/MyContactManager.kt
@@ -3,9 +3,9 @@ package com.sparta.seeseecallcall.data
 object MyContactManager {
     val myContact = Contact(
         name = "아이유",
-        mbti = "INFJ",
+        mbti = "ISFJ",
         phoneNumber = "010-1234-1234",
         birthDate = "1993/05/16",
-        email = "iu@gamil.com"
+        email = "iu@gmail.com"
     )
 }
